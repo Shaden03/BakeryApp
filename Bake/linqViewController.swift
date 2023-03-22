c6fcd4ec60bbdc0ef79851c7602ef21ebb3e35d2
Lina <Linahammad2002@gmail.com>
2023-03-22 17:38:11 +0300

    lina

---
+++ Bake/linqViewController.swift
@@ -0,0 +1,38 @@
+//
+//  linqViewController.swift
+//  Bake
+//
+//  Created by lina hammad aldahry on 30/08/1444 AH.
+//
+
+import UIKit
+
+class linqViewController: UIViewController {
+
+    @IBOutlet weak var LabelTitle: UILabel!
+    @IBOutlet weak var Image: UIImageView!
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var ChefName: UILabel!
+    @IBOutlet weak var Location: UILabel!
+    @IBOutlet weak var Date: UILabel!
+    @IBOutlet weak var Duration: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
